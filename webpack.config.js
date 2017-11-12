5ec8a28da8a441daf2feec338d46769b7b369871
Orehov Alexander <sorok@ukr.net>
2017-11-12 14:04:58 +0200

    Done with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 var path = require('path');
 
 module.exports = {
-  entry: './assets/js/index.js',
+  entry: './src/index.js', // файл который нужно обработать
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: 'bundle.js', // имя файла
+    path: path.resolve(__dirname, 'dist') // путь файла который будет собран
   },
   // Add the JSHint loader
   module: {
@@ -12,6 +12,16 @@ module.exports = {
       test: /\.js$/, // Выполнить загрузчик на всех файлах .js
       exclude: /node_modules/, // игнорировать все в папке node_modules
       use: 'jshint-loader'
+    },
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
     }]
   }
 };
