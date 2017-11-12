3151cf075e745240b3a6800df653bfa34c57808e
Orehov Alexander <sorok@ukr.net>
2017-11-12 19:32:59 +0200

    Add Babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,15 @@ module.exports = {
     filename: 'bundle.js', // имя файла
     path: path.resolve(__dirname, 'dist') // путь файла который будет собран
   },
-  // Add the JSHint loader
   module: {
     rules: [{
+       // Add the JSHint loader
       test: /\.js$/, // Выполнить загрузчик на всех файлах .js
       exclude: /node_modules/, // игнорировать все в папке node_modules
       use: 'jshint-loader'
     },
     {
+      // Add babel loader
       test: /\.js$/,
       exclude: /(node_modules|bower_components)/,
       use: {
